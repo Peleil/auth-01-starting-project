87eba934f342b6453d8e70f7625204cbe99d66e2
Yusang Park <usang.park@gmail.com>
2022-10-09 13:18:36 -0700

    using token as Id

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,8 +9,11 @@ export default {
       areas: data.areas,
     };
 
+    const token = context.rootGetters.token;
+
     const response = await fetch(
-      `https://main-prj-01-starting-setup-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      `https://main-prj-01-starting-setup-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=` +
+        token,
       {
         method: 'PUT',
         body: JSON.stringify(coachData),
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -28,8 +28,10 @@ export default {
   },
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId;
+    const token = context.rootGetters.token;
     const response = await fetch(
-      `https://main-prj-01-starting-setup-default-rtdb.firebaseio.com/requests/${coachId}.json`
+      `https://main-prj-01-starting-setup-default-rtdb.firebaseio.com/requests/${coachId}.json?auth=` +
+        token
     );
     const responseData = await response.json();
 
